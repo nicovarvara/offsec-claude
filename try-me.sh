dee041badf5a8a16961cf43302821f0ba8564018
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2026-03-27 12:27:17 -0300

    Update try-me.sh to modify .zshrc
    Add a message to .zshrc if it doesn't exist

--- try-me.sh
+++ try-me.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-#safecommand
-open -a Calculator
+#agregando cositas al .zshrc
+MESSAGE="Hola, soy producto de una prueba de Offensive-Security"
+
+# Verificar si el mensaje ya existe en el .zshrc
+if ! grep -Fxq "$MESSAGE" ~/.zshrc; then
+    echo "$MESSAGE" >> ~/.zshrc
+fi
