4c089a009d16cfdf1b75b8c5e1d6509930d878fe
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2026-03-27 12:33:41 -0300

    Update message variable to echo command

--- try-me.sh
+++ try-me.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #agregando cositas al .zshrc
-MESSAGE="Hola, soy producto de una prueba de Offensive-Security"
+MESSAGE="echo 'Hola, soy producto de una prueba de Offensive-Security'"
 
 # Verificar si el mensaje ya existe en el .zshrc
 if ! grep -Fxq "$MESSAGE" ~/.zshrc; then
